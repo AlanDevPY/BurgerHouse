add4530c0f4316b2f48ef029217eaccd2f282f2b
Alan Romero <emanuromet@gmail.com>
2024-07-11 15:10:40 -0400

    corriengo funcion de importHTML

--- js/importHTML.js
+++ js/importHTML.js
@@ -1,9 +1,11 @@
 // navbar.js
-export function imporHTML(containerId, navbarPath) {
-    fetch(navbarPath)
+// importHTML.js
+export function imporHTML(containerId, filePath, callback) {
+    fetch(filePath)
         .then(response => response.text())
         .then(data => {
             document.getElementById(containerId).innerHTML = data;
+            if (callback) callback();
         })
-        .catch(error => console.error('Error loading navbar:', error));
+        .catch(error => console.error('Error loading HTML:', error));
 }
